d067f7b79a3ae89cf5d6c4996e58515768e0574d
Jenny Cong <32729839+jfcong@users.noreply.github.com>
2025-02-10 20:23:54 -0800

    Revert "Add an input box and display the inputs in list below it."

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [list, setList] = useState([]);
-
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  const handleAddToList = () => {
-    if (inputValue.trim() !== ''){
-        setList([...list, inputValue]);
-        setInputValue('');
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('keypress', handleAddToList)
-  }, [list, inputValue]);
-
-
   return (
     <div className="App">
       <header className="App-header">
@@ -29,17 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleInputChange}
-          onKeyPress={handleAddToList}
-        />
-        <ul>
-          {list.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -54,4 +23,3 @@ function App() {
 }
 
 export default App;
-
